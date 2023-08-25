ac205d3a6f3541ee5132647122795eea477c29c5
Anastasia <anastasiakomova94@gmail.com>
2023-08-25 15:57:47 +0300

    начальная фиксация

---
+++ script.js
@@ -0,0 +1,69 @@
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+let slidere = document.getElementById("myRang");
+let outpute = document.getElementById("dem");
+outpute.innerHTML = slidere.value;
+slidere.oninput = function() {
+  outpute.innerHTML = this.value;
+}
+
+let slideri = document.getElementById("myRan");
+let outputi = document.getElementById("free");
+outputi.innerHTML = slideri.value;
+slideri.oninput = function() {
+outputi.innerHTML = this.value;
+}
+
+let slidera = document.getElementById("myRa");
+let outputa = document.getElementById("fo");
+outputa.innerHTML = slidera.value;
+slidera.oninput = function() {
+outputa.innerHTML = this.value;
+}
+
+let slide = document.getElementById("myR");
+let outpu = document.getElementById("fif");
+outpu.innerHTML = slide.value;
+slide.oninput = function() {
+outpu.innerHTML = this.value;
+}
+
+let slid = document.getElementById("my");
+let outp = document.getElementById("six");
+outp.innerHTML = slid.value;
+slid.oninput = function() {
+outp.innerHTML = this.value;
+}
+
+let sli = document.getElementById("m");
+let out = document.getElementById("seven");
+out.innerHTML = sli.value;
+sli.oninput = function() {
+out.innerHTML = this.value;
+}
+
+let sl = document.getElementById("gyRange");
+let ou = document.getElementById("eight");
+ou.innerHTML = sl.value;
+sl.oninput = function() {
+ou.innerHTML = this.value;
+}
+
+let s = document.getElementById("gyRang");
+let o = document.getElementById("nine");
+o.innerHTML = s.value;
+s.oninput = function() {
+o.innerHTML = this.value;
+}
+
+let sliderh = document.getElementById("gyRan");
+let outputh = document.getElementById("ten");
+outputh.innerHTML = sliderh.value;
+sliderh.oninput = function() {
+  outputh.innerHTML = this.value;
+}
